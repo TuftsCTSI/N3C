b807fca2a7c235662c88ec8758d91d235faef152
Polina Talapova <42737398+p-talapova@users.noreply.github.com>
2023-04-22 02:06:08 +0300

    Create db_conf.py

---
+++ scripts/SSSOM-to-OMOP-converter/db_conf.py
@@ -0,0 +1,10 @@
+# This section defines the database configuration
+db = {
+    "type": "postgresql",
+    "host": "localhost",
+    "port": "54321",
+    "user": "username",
+    "passwd": "password",
+    "db": "database",
+    "schema": "schema"
+}
